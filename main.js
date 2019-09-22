d9bbb88916bb6394d110ea45addc282ceb3c170e
Paul Nutlis <paulnu27@gmail.com>
2019-09-22 00:41:04 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+import { Questionarie } from "./model.js"
+console.log(JSON.stringify(Questionarie))
+// $(() => {
+//   // console.log(Questionarie)
+//   $('input[type=radio]').on('click', (e) => {
+//     let t = e.target
+//     console.log(t.value, t.id)
+//   })
+//   $('#calc').on('click', (e) => {
+//     let t = e.target
+//     console.log(t.id)
+//   })
+// })
---
+++ model.js
@@ -0,0 +1,40 @@
+
+export const Questionarie = [
+  { "id": "q1", "question": "To establish general pricing, would you consider your kitchen to be", "answer": "Small/Medium/Large", "input_type": "radio", "subquestions": [] },
+  { "id": "q2", "question": "Are you going to be replacing any drywall?", "answer": "Yes/No", "input_type": "radio", "subquestions": [] },
+  { "id": "q3", "question": "Will you be repainting your kitchen?", "answer": "Yes/No", "input_type": "radio", "subquestions": [] },
+  {
+    "id": "q4", "question": "Will you be adding or moving any electrical or electrical fixtures?", "answer": "Yes/No/Unsure", "input_type": "radio", "subquestions": [
+      {
+        "id": "q41", "question": "Will you be adding any new can lights, receptacles, or switches?", "answer": "Yes/No", "input_type": "radio", "subquestions": [
+
+          { "id": "q411", "question": "How many?", "answer": "", "input_type": "number", "subquestions": [] },
+        ]
+      },
+      {
+        "id": "q42", "question": "Will you be adding new pendant lights?", "answer": "Yes/No", "input_type": "radio", "subquestions": [
+
+          { "id": "q421", "question": "How many?", "answer": "", "input_type": "number", "subquestions": [] },
+        ]
+      },
+    ]
+  },
+  { "id": "q7", "question": "Are you going to relocate any plumbing?", "answer": "Yes/No", "input_type": "radio", "subquestions": [] },
+  { "id": "q8", "question": "Are you going to have a new sink", "answer": "Yes/No", "input_type": "radio", "subquestions": [] },
+  { "id": "q9", "question": "How about a new faucet?", "answer": "Yes/No", "input_type": "radio", "subquestions": [] },
+  { "id": "q10", "question": "What type of backplash are you going to have?", "answer": "", "input_type": "check", "subquestions": [] },
+  { "id": "q61", "question": "How many Square Feet?", "answer": "", "input_type": "number", "subquestions": [] },
+  { "id": "q11", "question": "What type of countertops are you going to have?", "answer": "", "input_type": "check", "subquestions": [] },
+  { "id": "q61", "question": "How many Square Feet?", "answer": "", "input_type": "number", "subquestions": [] },
+  { "id": "q12", "question": "Now let's pick out some cabinets. Here are your choices:", "answer": "Low Quality RTA Cabinets/Higher Quality RTA Cabinets/Assembled Stock Cabinets/Semi-Custom/High End Custom", "input_type": "check", "subquestions": [] },
+  {
+    "id": "q13", "question": "How tall would you like your upper cabinets to be?", "answer": "30inch T Uppers/36inch T Uppers/42inch T Uppers", "input_type": "check"
+  },
+  { "id": "q14", "question": "Moldings make a huge difference! What is your style?", "answer": "No moldings/Crown molding/Crown molding and light rail/The bells and whistles", "input_type": "radio", "subquestions": [] },
+  { "id": "q15", "question": "How many linear feet of upper cabinets do you have?", "answer": "", "input_type": "number", "subquestions": [] },
+  { "id": "q16", "question": "For your base cabinets, would you like?", "answer": "standard base cabinets (lower doors and upper drawers)/A mixture of standard and all drawer bases/Lots of Drawers and pullouts", "input_type": "radio", "subquestions": [] },
+  { "id": "q17", "question": "How many linear feet of lower cabinets do you have?", "answer": "", "input_type": "number", "subquestions": [] },
+  { "id": "q18", "question": "What type of floor is your home built with?", "answer": "concrete slab/wood/framed floor", "input_type": "radio", "subquestions": [] },
+  { "id": "q19", "question": "You are almost done! We just need your flooring. What type of flooring would you like? (About ten different options with pictures here)", "answer": "", "input_type": "check", "subquestions": [] },
+  { "id": "q20", "question": "How many square feet of flooring are you going to install?", "answer": "", "input_type": "number", "subquestions": [] },
+]
