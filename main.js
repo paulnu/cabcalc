efb761a6aed8b62737abe967c17786e984d8264b
paulnu <paulnu27@gmail.com>
2019-09-23 15:57:59 -0500

    dev: working on subquestions toggle

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ $(() => {
   const hbs = Handlebars
   const src = $('#entry-template').html()
   const template = hbs.compile(src)
-  const partials = ['main', 'icheck', 'inumber', 'itext', 'iradio']
+  const partials = ['main', 'question.item', 'icheck', 'inumber', 'itext', 'iradio']
   const promises = []
 
   hbs.registerHelper('isNumberType', (data, options) => {
@@ -23,11 +23,23 @@ $(() => {
     return data.input_type === 'check' ? options.fn(data) : options.inverse(data)
   })
 
+  hbs.registerHelper('areSubquestions', (data, options) => {
+    return data.subquestions && data.subquestions.length > 0 ? options.fn(data) : options.inverse(data)
+  })
+
   partials.map((p) => {
     promises.push($.get(`partials/${p}.hbs`, (data) => {
       hbs.registerPartial(p, data)
     }, 'html'))
   })
+
+  function toggleSubQuestions(id) {
+    let el = $(`#${id}`)
+    // el.children('input[type=radio]').on('change', (e) => {
+    //   console.log(e.target.val())
+    // })
+    // el.children().hide()
+  }
 
   Promise.all(promises).then(
     $.getJSON("questionarie.json")
@@ -36,11 +48,28 @@ $(() => {
           questions: data
         })
         $('#content').html(content)
-
-        // $('input[type=radio]').on('click', (e) => {
-        //   let t = e.target
-        //   console.log(t.value, t.id)
-        // })
+        let subs = data.filter((i) => i.subquestions && i.subquestions.length > 0)
+        //let subs1 = subs.subquestions.filter((i) => i.subquestions && i.subquestions.length > 0)
+        subs.map((s) => {
+          // $(`input[name=radio_${s.id}]`).change((e) => {
+          //   console.log(e.target.value)
+          // })
+          // console.log(s.id, s.show_subquestions_trigger, s.subquestions)
+          s.subquestions.map((i) => toggleSubQuestions(i.id))
+          let subs1 = s.subquestions.filter((i) => i.subquestions && i.subquestions.length > 0)
+          subs1.map((s) => {
+            // console.log(s.id, s.show_subquestions_trigger)
+            s.subquestions.map((i) => toggleSubQuestions(i.id))
+            let subs2 = s.subquestions.filter((i) => i.subquestions && i.subquestions.length > 0)
+            subs2.map((s) => {
+              // console.log(s.id, s.show_subquestions_trigger)
+            })
+          })
+        })
+        $('input[type=radio]').on('click', (e) => {
+          let t = e.target
+          console.log(t.value, t.id)
+        })
         // $('#calc').on('click', (e) => {
         //   let t = e.target
         //   console.log(t.id)
